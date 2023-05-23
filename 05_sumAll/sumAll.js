38d28a27f3b05eea5cd690992ee812acfb6502b3
Ansh Raiyani <anshraiyani13@gmail.com>
2023-05-23 15:57:20 +0530

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    if((start<0 || end <0) || typeof(start)!="number" || typeof(end)!="number") return "ERROR"
+    var sum=0
+    var s=Math.min(start,end)
+    var e=Math.max(start,end)
+    while(s<=e){
+        sum+=s
+        s++
+    }
+    return sum
 };
 
 // Do not edit below this line
